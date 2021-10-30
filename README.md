e56358e4665155a1505ebc853b5fa0128ce9ece4
John Davidson <jhd1013@msn.com>
2021-10-30 11:32:39 -0500

    Completed Module 11 challenge.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# UFOs
-Data Analytics Boot Camp Module 11
+# UFO Sightings
+## Purpose
+The purpose of this project is to provide an easily accessible data set describing UFO sightings to anyone with Internet access.  In addition to providing easy access, this page also provides a simple way to filter by date, location, and type of sighting to make the large data set easier to use.
+
+## Resources
+- Source Data:  [UFO Data](./static/js/data.js)
+- Tools Used:  Javascript ES6+, Bootstrap, D3, HTML
+
+## Usage
+The data on the page is very easy to filter.  Simply enter a date, location, and/or UFO shape into the provide text box, hit the enter key or move away from the box (MS Edge) and the table will automatically update.  Below are some examples.
+
+To filter on date only, enter the date using the format show in the box (Month/Day/Year).  The year should be entered as 4-digits.  To the right of the filter you can see that the table has been updated to show only sightings from January 5, 2010
+
+![Filter by Date only](./static/images/date-only.png)
+
+To filter on multiple criteria, update another filter item while leaving the existing filter(s) in place.  Below shows the addition of one more filter for state.  The table is updated to show sightings from New Jersey (nj) on January 5, 2010.
+
+![Filter by Date and State](./static/images/date-state.png) 
+
+One more example that filters on date, state, and UFO shape.
+
+![Filter by Date, State, and Shape](./static/images/date-state-shape.png) 
+
+## Recommendations for Improvement
+There are a number of improvements that could be made to enhance the usability of this page:
+- Provide a means to filter by partial date, such as by year, month, or day to allow users to find patterns in timing.
+- Prior to filtering, convert all text to the same case (upper or lower) to eliminate the risk of a failed match due to differing case.
+- Add summary information to help users decide where to start on their search.  For example, which countries, states, shapes, and date ranges are available.
+- For the date filter, convert all dates to a common date/time format prior to filtering.  Similar to the case issue previously described, this will help minimize poor filter results due to differences in how users represent dates.
--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,85 @@
-// import the data from data.js
+// from data.js
 const tableData = data;
 
-// Reference the HTML table using d3
+// get table references
 var tbody = d3.select("tbody");
 
-function buildTable(data){
-    tbody.html("");
-    // Create a row for each object in the data array
-    data.forEach((dataRow) => {
-        let row = tbody.append("tr");
-        // Insert each item in this object into a <td> tag
-        Object.values(dataRow).forEach((val) => {
-            let cell = row.append("td")
-            cell.text(val); 
-        });
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
+  });
 }
 
-function handleClick() {
-    let date = d3.select("#datetime").property("value");
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+    let filterElement = d3.select(this);
+
+    // 4b. Save the value that was changed as a variable.
+    let filterValue = filterElement.property("value");
+    console.log(filterValue)
+
+    // 4c. Save the id of the filter that was changed as a variable.
+    let filterId = filterElement.property("id");
+    console.log(filterId)
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (filterValue){
+        filters[filterId] = filterValue;
+    } else {
+        delete filters[filterId];
+    }
+ 
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
+  }
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
-    if (date) {
-        // if a date is provided, filter the data to just that date
-        filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    let filterIds = Object.keys(filters)
+    let filterVals = Object.values(filters)
+    for (let i = 0; i < filterIds.length; i++){
+        filteredData = filteredData.filter(row => row[filterIds[i]] === filterVals[i])
+        console.log(filterIds[i]);
+        console.log(filterVals[i]);
+    }
+    
+    // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-}
+    
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("#datetime").on("change", updateFilters);  
+  d3.selectAll("#city").on("change", updateFilters);  
+  d3.selectAll("#state").on("change", updateFilters);  
+  d3.selectAll("#country").on("change", updateFilters);  
+  d3.selectAll("#shape").on("change", updateFilters);
 
-d3.selectAll("#filter-btn").on("click", handleClick);
-buildTable(tableData)
+  // Build the table when the page loads
+  buildTable(tableData);
